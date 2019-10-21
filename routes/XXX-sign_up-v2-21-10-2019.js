3ed3ace0c3256271b79e2e4b4349fa96428b5421
Cripto Girl <natalie.seltzer@gmail.com>
2019-10-21 17:47:59 +0100

    getting google refresh_token

--- routes/XXX-sign_up-v2-21-10-2019.js
+++ routes/XXX-sign_up-v2-21-10-2019.js
@@ -1,62 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const db = require('ocore/db');
-// Google API libraries integration
+// Google API libraries
 const {google} = require('googleapis');
-const url = require('url');
 
 router.get('/', (req, res) => {
-
-  // Google integration
-  // check if the calling url contains response from Google
-  var q = url.parse(req.url, true);
-  var qdata = q.query; //returns an object: {code: '4/sQF_....' }
-  if (qdata.code) {
-    // Google Authorisation page returned authorisation code, e.g.
-    // code=4/sQF_IxMK5-GF-wBlOPNyJtzVg-kKg2lpFU1hCWBJ5lSC7tP1R9KdcYu_sYKFTqp5h-OuO_zBEXf83nSal7Y1psw&scope=https://www.googleapis.com/auth/fitness.activity.read
-    let google_authorization_code = qdata.code;
-    //Retrieve access token
-    // This will provide an object with the access_token and refresh_token.
-    //const {tokens} = await oauth2Client.getToken(google_authorization_code)
-    const {tokens} = oauth2Client.getToken(google_authorization_code);
-    oauth2Client.setCredentials(tokens);
-    const refresh_token = tokens.refresh_token;
-    res.send('refresh_token: '+ refresh_token);
-    //res.render('obyfit/sign_up', { title: 'ObyFit Challenge Sign-up page', google_authorization_code, tokens });
-  }
-  else if (qdata.error) {
-    // Google Authorisation page returned an error
-  }
-  else {
-    //res.send('qdata code is false: '+qdata.code);
-    let google_auth_request_pg = true;
-    res.render('obyfit/sign_up', { title: 'ObyFit Challenge Sign-up page', google_auth_request_pg });
-  }
+  //res.send('ObyFit Challenge Sign-up page');
+  let user_data_form = true;
+  res.render('obyfit/sign_up', { title: 'ObyFit Challenge Sign-up page', user_data_form });
 });
 
 router.post('/', (req, res) => {
   //
   let error, result;
   let wallet = req.body.wallet;
-  let google_authorization_code = req.body.google_authorization_code;
-  let form_action = req.body.form_action;
+  let email = req.body.email;
+  let state = req.body.state;
   // saving user Data
   // check that wallet address is a valid address
   // check that we do not have this record already
-  if (form_action === 'save_user_data') {
+  if (state === 'sign_up') {
     db.query(`INSERT ` + db.getIgnore() + ` INTO xwf_obyfit_user_challenge
-      ( wallet, google_authorization_code ) VALUES (?,?)`,
-      [ wallet, google_authorization_code ],
+      ( wallet, email ) VALUES (?,?)`, [ wallet, email ],
       function (response) {
         if (response.insertId && response.affectedRows) result = 'User data has been sucessfully saved';
         else error = 'Can not save user data';
-        res.render('obyfit/sign_up', { title: 'User data is saved', error, result, wallet });
+        res.render('obyfit/sign_up', { title: 'User data is saved', error, result, wallet, email });
     });
   }
 
   // getting authorisation from Google for Google Fit API connection
-  if (form_action === 'authorise') {
-    let authorise = form_action;
+  if (state === 'authorise') {
+    let authorise = state;
     // google
     const obyfit_client_id = '109013719177-cfsh1i1gla7nhq9pevcuj80t0h55ud0d.apps.googleusercontent.com';
     const obyfit_client_secret = 'YpQYwKJIV3wx6flgfLu6uhIW';
@@ -70,14 +45,14 @@ router.post('/', (req, res) => {
     const scopes = [
       'https://www.googleapis.com/auth/fitness.activity.read'
     ];
-    const googleURL = oauth2Client.generateAuthUrl({
+    const url = oauth2Client.generateAuthUrl({
       // 'online' (default) or 'offline' (gets refresh_token)
       access_type: 'offline',
       // If you only need one scope you can pass it as a string
       scope: scopes
     });
 
-    res.redirect(googleURL);
+    res.redirect(url)
     //
     //res.render('obyfit/sign_up', { title: 'User data is saved', error, result, wallet, email, authorise });
   }
