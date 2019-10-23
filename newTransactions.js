c0308ab3c72c76eee76d527bcc29b6ddf5bd8f8a
Cripto Girl <natalie.seltzer@gmail.com>
2019-10-23 14:44:01 +0100

    fixing errors

--- newTransactions.js
+++ newTransactions.js
@@ -8,7 +8,7 @@ function newTransactions(arrUnits) {
   // for each new transaction unit
   for(let i=0; i<arrUnits.length; i++) {
     let unit = arrUnits[i];
-    db.query(`SELECT 1 FROM unit_authors WHERE unit=?AND address=?`,
+    db.query(`SELECT 1 FROM unit_authors WHERE unit=? AND address=?`,
       [unit, config.aaAddress], outboundTxn => {
         //if (outboundTxn.length === 1) // outbound Transaction
         if (outboundTxn.length === 0) {  // inbound Transaction
