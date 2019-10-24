a66fdfd30f38e46a8574aa80bea2e14c258ce26a
Cripto Girl <natalie.seltzer@gmail.com>
2019-10-24 17:12:03 +0100

    add test harness

--- routes/admin_edit.js
+++ routes/admin_edit.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('ocore/db');
-// Config
+// Obyfit Config & modules
 const config = require('../conf_game.js');
+const aa_data = require('../aa_data.js');
 
-function getUserData(res) {
+function getUserData(res, message) {
   // get user data from db
   let error, result;
   db.query(`SELECT
@@ -14,7 +15,7 @@ function getUserData(res) {
     userRows => {
       if (userRows.length === 0) error = 'No data found';
       else result = userRows;
-    res.render('obyfit/admin_edit', { title: 'ObyFit Admin Edit page', error, result });
+    res.render('obyfit/admin_edit', { title: 'ObyFit Admin Edit page', error, result, message });
   });
 }
 
@@ -35,21 +36,40 @@ router.post('/', (req, res) => {
         else if (rows.length > 1) error = 'Duplicate rows found';
         else {
           let data = rows[0];
+          let new_period = data.latest_day_nb + 1;
           res.render('obyfit/admin_edit', { title: 'ObyFit Admin Edit page',
-            edit_form, data });
+            edit_form, data, new_period });
         }
     });
   }
 
   if (req.body.form_action === 'save') {
-    db.query(`UPDATE xwf_obyfit_user_challenge SET
-      latest_day_nb=?, latest_day_step_count=?, total_step_count=?, updated_reason=?
-      WHERE wallet=?`,
-      [req.body.latest_day_nb, req.body.latest_day_step_count, req.body.total_step_count,
-      req.body.updated_reason, req.body.wallet ]);
-    getUserData(res);
-  }
+    let message;
+    db.query(`SELECT wallet, total_step_count
+      FROM xwf_obyfit_user_challenge WHERE wallet=?`, [req.body.wallet],
+      rows => {
+        if (rows.length === 0) message = 'No data found';
+        else if (rows.length > 1) message = 'Duplicate rows found';
+        if (rows.length === 1) {
+          let row = rows[0];
+          let total_step_count = row.total_step_count + Number(req.body.latest_day_step_count);
+          let updated_reason;
+          if (req.body.new_period === '7') updated_reason = 'Ended';
+          else updated_reason = 'Running';
 
+          db.query(`UPDATE xwf_obyfit_user_challenge SET
+            latest_day_nb=?, latest_day_step_count=?, total_step_count=?, updated_reason=?
+            WHERE wallet=?`,
+            [req.body.new_period, req.body.latest_day_step_count, total_step_count,
+            updated_reason, req.body.wallet ]);
+          // send step count to AA
+          //aa_data.sendSteps(req.body.wallet, req.body.latest_day_step_count,
+          //  req.body.latest_day_nb, total_step_count);
+          message='Data sucessfully updated and step count sent to AA.'
+        }
+      getUserData(res, message);
+    });
+  }
 });
 
 module.exports = router;
